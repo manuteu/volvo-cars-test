2252dc234f7721ec68456b5bd6956a393e7c9e86
Matheus <matheusim27@hotmail.com>
2023-07-07 10:55:06 -0300

    add: fix lint errors

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -62,7 +62,7 @@ const Cars = () => {
         <div className="relative w-full flex items-center h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
           <div className="grid place-items-center h-full w-12 text-[#626262] bg-[#F2F2F5]">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
           </div>
           <input
@@ -88,9 +88,8 @@ const Cars = () => {
         <Carousel>
           {dataCars.map((item, index) => {
             return (
-              <div className="mr-6">
+              <div className="mr-6" key={index}>
                 <Card
-                  key={index}
                   imageUrl={item.imageUrl}
                   modelName={item.modelName}
                   modelType={item.modelType}
